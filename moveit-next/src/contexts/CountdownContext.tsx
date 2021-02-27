64f63940f01ddbc317edf26caa85b5e67ed125a5
Fernanda Penna <fernandapennaribeiro@gmail.com>
2021-02-27 13:32:57 -0300

    add readme

--- moveit-next/src/contexts/CountdownContext.tsx
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -25,7 +25,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge } = useContext(challengesContext)
 
     // colocaremos o tempo em segundos
-    const [time, setTime] = useState(0.1 * 60)
+    const [time, setTime] = useState(25 * 60)
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
@@ -42,7 +42,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
         clearTimeout(countdownTimeout);
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.1 * 60);
+        setTime(25 * 60);
       }
 
 
